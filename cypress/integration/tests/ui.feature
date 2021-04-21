786967ae8fe816557b4c9c9008c3d8a0512c16e3
jitendrabdev <jitendradev@gmail.com>
2021-04-21 14:48:58 +0200

    Added new code for more scenarios

--- cypress/integration/tests/ui.feature
+++ cypress/integration/tests/ui.feature
@@ -17,7 +17,7 @@ Scenario: User should create new advertisement with valid data
     |advertisementName|street |rooms|price|status|
     | advertisement1  |Street1| 23  | 12  | true |
 
-  Scenario: User should edit advertisement created
+Scenario: User should edit advertisement created
     When I click on existing advertisement
     Then I see edit advertisement page
     When I modify any attribute
@@ -30,4 +30,19 @@ Scenario: User should create new advertisement with valid data
     |advertisementName|street |rooms|price|status|
     | advertisement2  |Street2| 24  | 13  | true |
 
+Scenario Outline: Field validation on create advertisement page
+    When I click on new advertisement button
+    Then I see new advertisement page
+    When I provide '<advertisementName>', '<street>', '<rooms>', '<price>'
+    Then I see Error message for advertisement as '<advertisementError>' and for price as '<priceError>'
+    And  I see Cancel button '<cancelButton>'
+    And  I see Save button '<saveButton>'
 
+Examples:
+|advertisementName                                  |advertisementError         |street  |rooms|price|priceError                                    |status|cancelButton|saveButton|
+|$aa                                                |Alphabets and numerals only|street1 |12   | 50  |                                              |Active|enabled     | disabled |
+|abc                                                |                           |street1 |12   |test | Invalid price(Valid currency in euros: 12,12)|Active|enabled     | disabled |
+|asfadgdfgfhgjhkhljklkhhdhfdsdfasfasfasfsdgdfhdfhdfh|Max length reached         |street1 |12   |100  |                                              |Active|enabled     | disabled |
+|abc                                                |                           |street1 |12   |12.1 | Invalid price(Valid currency in euros: 12,12)|Active|enabled     | disabled |
+|                                                   |This is required           |street1 |12   |20   |                                              |Active|enabled     | disabled |
+|abc                                                |                           |street1 |12   |     | This is required                             |Active|enabled     | disabled |
--- cypress/integration/tests/ui/ui-steps.js
+++ cypress/integration/tests/ui/ui-steps.js
@@ -1,14 +1,6 @@
 /// <reference types="cypress" />
 
-advertisement1 = "";
-
-//generate a random number to create an advertisement,
-//to be added with the initial String
-function generateRandom() {
-  return Math.floor((Math.random() + 1) * 10);
-}
-
-let advertisement1 = "ADD" + generateRandom();
+let advertisement1 = "";
 
 Given("I navigate to advertisement page", () => {
   cy.visit("/");
@@ -24,35 +16,129 @@ When("I click on new advertisement button", () => {
 
 Then("I see new advertisement page", () => {
   cy.get(".md-subhead").should("be.visible").contains("Advertisement");
-  cy.get("#input_0").should("be.visible");
-  cy.get("#input_1").should("be.visible");
-  cy.get("#input_2").should("be.visible");
-  cy.get("#input_3").should("be.visible");
+  cy.get('[ng-model="$ctrl.advertisement.name"]').should("be.visible");
+  cy.get('[ng-model="$ctrl.advertisement.street"]').should("be.visible");
+  cy.get('[ng-model="$ctrl.advertisement.rooms"]').should("be.visible");
+  cy.get('[ng-model="$ctrl.advertisement.price"]').should("be.visible");
   cy.get(".md-container").should("be.visible");
-  cy.get('[ng-click="$ctrl.cancel()"] > .ng-scope').should("be.visible");
-  //cy.get('[ng-disabled="$ctrl.disableSave"] > .ng-scope').should("be.disabled");
+  cy.get('[ng-click="$ctrl.cancel()"]').should("be.visible");
+  //cy.get('[ng-disabled="$ctrl.disableSave"]').should("be.disabled");
 });
 
-And("I type all value of attributes", () => {
-  cy.get("#input_0").type(advertisement1);
-  cy.get("#input_1").type("test");
-  cy.get("#input_2").type("12");
-  cy.get("#input_3").type("1201");
+And("I type all value of attributes", (datatable) => {
+  datatable.hashes().forEach((row) => {
+    cy.get('[ng-model="$ctrl.advertisement.name"]').type(row.advertisementName);
+    advertisement1 = row.advertisementName;
+    cy.get('[ng-model="$ctrl.advertisement.street"]').type(row.street);
+    cy.get('[ng-model="$ctrl.advertisement.rooms"]').type(row.rooms);
+    cy.get('[ng-model="$ctrl.advertisement.price"]').type(row.price);
+  });
 });
 
 And("I click on Save button", () => {
-  cy.get('[ng-disabled="$ctrl.disableSave"] > .ng-scope').click();
+  cy.get('[ng-disabled="$ctrl.disableSave"]').click();
 });
 
 Then("I see advertisement added message", () => {
   cy.get(".md-toast-text").should("be.visible").contains("Saved successful");
 });
 
-And("I verify that all attributes are correctly displayed", () => {
+And("I verify that all attributes are correctly displayed", (datatable) => {
   cy.wait(2000);
   cy.contains(advertisement1).click();
-  cy.get("#input_5").should("contain.value", advertisement1);
-  cy.get("#input_6").should("contain.value", "test");
-  cy.get("#input_7").should("contain.value", "12");
-  cy.get("#input_8").should("contain.value", "1201");
+  datatable.hashes().forEach((row) => {
+    cy.get('[ng-model="$ctrl.advertisement.name"]').should(
+      "contain.value",
+      advertisement1
+    );
+    cy.get('[ng-model="$ctrl.advertisement.street"]').should(
+      "contain.value",
+      row.street
+    );
+    cy.get('[ng-model="$ctrl.advertisement.rooms"]').should(
+      "contain.value",
+      row.rooms
+    );
+    cy.get('[ng-model="$ctrl.advertisement.price"]').should(
+      "contain.value",
+      row.price
+    );
+  });
+});
+
+When("I click on existing advertisement", () => {
+  cy.contains(advertisement1).click();
+});
+
+Then("I see edit advertisement page", () => {
+  cy.url().should("include", "/edit");
+});
+
+When("I modify any attribute", (datatable) => {
+  cy.get('[ng-model="$ctrl.advertisement.name"]').clear();
+  cy.get('[ng-model="$ctrl.advertisement.street"').clear();
+  cy.get('[ng-model="$ctrl.advertisement.rooms"]').clear();
+  cy.get('[ng-model="$ctrl.advertisement.price"]').clear();
+  datatable.hashes().forEach((row) => {
+    cy.get('[ng-model="$ctrl.advertisement.name"]').type(row.advertisementName);
+    advertisement1 = row.advertisementName;
+    cy.get('[ng-model="$ctrl.advertisement.street"').type(row.street);
+    cy.get('[ng-model="$ctrl.advertisement.rooms"]').type(row.rooms);
+    cy.get('[ng-model="$ctrl.advertisement.price"]').type(row.price);
+  });
+});
+
+Then("I see save button is enabled", () => {
+  cy.get('[ng-disabled="$ctrl.disableSave"]').should("be.enabled");
+});
+
+When("I click on Save button", () => {
+  cy.get('[ng-disabled="$ctrl.disableSave"]').click();
+});
+
+Then("I see advertisement added message", () => {
+  cy.get(".md-toast-text").should("be.visible").contains("Saved successful");
+});
+
+When(
+  "I provide {string}, {string}, {string}, {string}",
+  (advertisementName, street, rooms, price) => {
+    cy.get('[ng-model="$ctrl.advertisement.name"]').clear();
+    cy.get('[ng-model="$ctrl.advertisement.street"').clear();
+    cy.get('[ng-model="$ctrl.advertisement.rooms"]').clear();
+    cy.get('[ng-model="$ctrl.advertisement.price"]').clear();
+    if (advertisementName) {
+      cy.get('[ng-model="$ctrl.advertisement.name"]').type(advertisementName);
+    }
+    cy.get('[ng-model="$ctrl.advertisement.street"').type(street);
+    cy.get('[ng-model="$ctrl.advertisement.rooms"]').type(rooms);
+    if (price) {
+      cy.get('[ng-model="$ctrl.advertisement.price"]').type(price);
+    }
+  }
+);
+
+Then(
+  "I see Error message for advertisement as {string} and for price as {string}",
+  (advertisementError, priceError) => {
+    if (advertisementError) {
+      cy.get(
+        ":nth-child(1) > .md-input-messages-animation > .md-input-message-animation"
+      ).should("contain", advertisementError);
+    }
+    if (priceError) {
+      cy.get(
+        ":nth-child(4) > .md-input-messages-animation > .md-input-message-animation"
+      ).should("contain", priceError);
+    }
+  }
+);
+And("I see Cancel button {string}", (cancelButton) => {
+  cy.get('[ng-click="$ctrl.cancel()"]').should("be." + cancelButton);
+});
+
+And("I see Save button {string}", (saveButton) => {
+  cy.get('[ng-disabled="$ctrl.disableSave"]').should("be." + saveButton);
+  cy.get('[ng-click="$ctrl.cancel()"]').click();
+  cy.get(".md-confirm-button").click();
 });
